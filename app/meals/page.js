fae35229bd3611751555ac1a676b802fe04d4763
lukasz <l.bielecki13@gmail.com>
2024-05-28 22:55:20 +0200

    adding meals-grid and meal-item components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+
+import MealsGrid from '@/components/meals/meals-grid';
+
 import styles from './page.module.css';
 
 export default function MealsPage() {
@@ -13,7 +16,9 @@ export default function MealsPage() {
           <Link href='/meals/share'>Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
