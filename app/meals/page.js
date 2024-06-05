ab0615bad861bbae5acb5403779bf2138cbc451f
lukasz <l.bielecki13@gmail.com>
2024-06-05 21:50:35 +0200

    adding loader for meals only

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,14 +1,20 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 
 import MealsGrid from '@/components/meals/meals-grid';
+import MealsLoadingPage from './loading-out';
 
 import { getMeals } from '@/lib/meals';
 
 import styles from './page.module.css';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -21,7 +27,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
